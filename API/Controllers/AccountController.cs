e010372097ac2b3b86637cb0b17755ac9f87ac48
Admin <Admin@DESKTOP-8QAFHBA>
2025-07-25 13:23:22 +0400

    Creating an Account Controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context) : BaseApiController
+{
+    [HttpPost("register")] // api/account/register
+    public async Task<ActionResult<AppUser>> Register(string email, string displayName, string password)
+    {
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            DisplayName = displayName,
+            Email = email,
+            PassworHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            PassworSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user;
+    }
+
+}
---
+++ API/Data/Migrations/20250725090203_UserEntityUpdate.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserEntityUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PassworSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PassworHash",
+                table: "Users",
+                type: "BLOB",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "PassworSalt",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "BLOB");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "PassworHash",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "BLOB");
+        }
+    }
+}

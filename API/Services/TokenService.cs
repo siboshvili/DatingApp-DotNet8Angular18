018959e300db3510d2bbdfa767a959ac64069427
siboshvili <siboshvili.andro@gmail.com>
2025-07-26 12:01:43 +0400

    Implementing the token logic.

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,12 +1,39 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services;
 
-public class TokenService : ITokenService
+public class TokenService(IConfiguration config) : ITokenService
 {
-    public string CreateToken(AppUser appUser)
+    public string CreateToken(AppUser user)
     {
-        throw new NotImplementedException();
+        var tokenKey = config["Token"] ?? throw new Exception("Cannot get token key");
+        if (tokenKey.Length < 64)
+            throw new Exception("Your token key needs to be >= 64 characters");
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Email, user.Email),
+            new(ClaimTypes.NameIdentifier, user.Id)
+        };
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDesctiptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDesctiptor);
+
+        return tokenHandler.WriteToken(token);
     }
 }

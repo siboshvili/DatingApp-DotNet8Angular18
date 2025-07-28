894b5e800406b8c278afb525381101b980f6916a
siboshvili <siboshvili.andro@gmail.com>
2025-07-28 12:54:03 +0400

    feat(auth): integrate AccountService with login form

--- client/src/layout/nav/nav.component.ts
+++ client/src/layout/nav/nav.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { AccountService } from '../../core/services/account.service';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  private accountService = inject(AccountService);
+  protected creds: any = {}
+
+  login() {
+    this.accountService.login(this.creds).subscribe({
+      next: result =>console.log(result),
+      error: error => console.log(error)
+    });
+  }
+
 
 }

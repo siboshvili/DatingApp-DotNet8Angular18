f41c26d0363618ca35c6b3e82f3506bfbbb861e7
siboshvili <siboshvili.andro@gmail.com>
2025-07-29 13:30:41 +0400

    feat(auth): show toast messages for login success and failure.

--- client/src/layout/nav/nav.component.ts
+++ client/src/layout/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AccountService } from '../../core/services/account.service';
+import { ToastService } from '../../core/services/toast.service';
 import {Router, RouterLink, RouterModule} from '@angular/router';
 
 @Component({
@@ -13,15 +14,19 @@ import {Router, RouterLink, RouterModule} from '@angular/router';
 export class NavComponent {
   protected accountService = inject(AccountService);
   private router = inject(Router);
+  private toastService = inject(ToastService);
   protected creds: any = {}
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: result => {
         this.router.navigateByUrl('/members');
+        this.toastService.success('Login successful');
         this.creds = {};
       },
-      error: error => console.log(error)
+      error: error => {
+        this.toastService.error(error.error);
+      }
     });
   }
 

c756afd44bad1b3a487b2004107060a0dbaf88e6
siboshvili <siboshvili.andro@gmail.com>
2025-07-31 13:42:00 +0400

    feat(member): link Member to AppUser via navigation and foreign key.

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -8,4 +8,7 @@ public class AppUser
     public string? ImageUrl { get; set; }
     public required byte[] PasswordHash { get; set; }
     public required byte[] PasswordSalt { get; set; }
+    
+    // Nav property
+    public Member Member { get; set; }
 }

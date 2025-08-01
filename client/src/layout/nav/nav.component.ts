ed76f098585946963429d62d2e1c492ac503ed69
siboshvili <siboshvili.andro@gmail.com>
2025-08-01 23:13:06 +0400

    feat: integrate DaisyUI themes.

--- client/src/layout/nav/nav.component.ts
+++ client/src/layout/nav/nav.component.ts
@@ -1,38 +1,54 @@
-import { Component, inject, signal } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import { AccountService } from '../../core/services/account.service';
-import { ToastService } from '../../core/services/toast.service';
+import {AccountService} from '../../core/services/account.service';
+import {ToastService} from '../../core/services/toast.service';
 import {Router, RouterLink, RouterModule} from '@angular/router';
+import { themes } from '../theme';
+
 
 @Component({
-  selector: 'app-nav',
-  standalone: true,
-  imports: [FormsModule, RouterLink, RouterModule],
-  templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+    selector: 'app-nav',
+    standalone: true,
+    imports: [FormsModule, RouterLink, RouterModule],
+    templateUrl: './nav.component.html',
+    styleUrl: './nav.component.css'
 })
-export class NavComponent {
-  protected accountService = inject(AccountService);
-  private router = inject(Router);
-  private toastService = inject(ToastService);
-  protected creds: any = {}
+export class NavComponent implements OnInit {
+    protected accountService = inject(AccountService);
+    private router = inject(Router);
+    private toastService = inject(ToastService);
+    protected creds: any = {}
+    protected selectedTheme = signal<string>(localStorage.getItem('theme') || 'light');
+    protected themes = themes;
 
-  login() {
-    this.accountService.login(this.creds).subscribe({
-      next: () => {
-        this.router.navigateByUrl('/members');
-        this.toastService.success('Login successful');
-        this.creds = {};
-      },
-      error: error => {
-        this.toastService.error(error.error);
-      }
-    });
-  }
+    ngOnInit(): void {
+        document.documentElement.setAttribute('data-theme', this.selectedTheme());
+    }
 
-  logout() {
-    this.accountService.logout();
-    this.router.navigateByUrl('/');
-  }
+    handleSelectTheme(theme: string) {
+        this.selectedTheme.set(theme);
+        localStorage.setItem('theme', theme);
+        document.documentElement.setAttribute('data-theme', theme);
+        const elem = document.activeElement as HTMLDivElement;
+        if (elem) elem.blur();
+    }
+
+    login() {
+        this.accountService.login(this.creds).subscribe({
+            next: () => {
+                this.router.navigateByUrl('/members');
+                this.toastService.success('Login successful');
+                this.creds = {};
+            },
+            error: error => {
+                this.toastService.error(error.error);
+            }
+        });
+    }
+
+    logout() {
+        this.accountService.logout();
+        this.router.navigateByUrl('/');
+    }
 
 }

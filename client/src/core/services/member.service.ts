ad10abe309cbff310701de6e15bf597f6c099c68
siboshvili <siboshvili.andro@gmail.com>
2025-08-04 12:59:20 +0400

    feat(member): implement member profile update in client app.

--- client/src/core/services/member.service.ts
+++ client/src/core/services/member.service.ts
@@ -2,6 +2,8 @@ import {inject, Injectable, signal} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment.development";
 import {Member, Photo} from "../../types/member";
+import {EditableMember} from "../../types/member";
+import { tap } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -10,16 +12,26 @@ export class MemberService {
     private http = inject(HttpClient);
     private baseUrl = environment.apiUrl;
     editMode = signal(false);
+    member = signal<Member | null>(null);
 
     getMembers() {
         return this.http.get<Member[]>(this.baseUrl + 'members');
     }
 
     getMember(id: string) {
-        return this.http.get<Member>(this.baseUrl + 'members/' + id);
+        return this.http.get<Member>(this.baseUrl + 'members/' + id).pipe(
+            tap(member => {
+                this.member.set(member);
+            })
+        );
     }
 
     getMemberPhotos(id: string) {
         return this.http.get<Photo[]>(this.baseUrl + 'members/' + id + '/photos');
     }
+    
+    updateMember(member: EditableMember) {
+        return this.http.put(this.baseUrl + 'members/', member);
+    }
+        
 }
--- client/src/features/members/member-profile/member-profile.component.ts
+++ client/src/features/members/member-profile/member-profile.component.ts
@@ -1,10 +1,10 @@
 import {Component, HostListener, inject, OnDestroy, OnInit, signal, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
 import {EditableMember, Member} from '../../../types/member';
 import {DatePipe} from "@angular/common";
 import { MemberService } from '../../../core/services/member.service';
 import {FormsModule, NgForm} from "@angular/forms";
-import {ToastService} from '../../../core/services/toast.service';
+import {ToastService} from '../../../core/services/toast.service'
+import {AccountService} from '../../../core/services/account.service';
 
 @Component({
     selector: 'app-member-profile',
@@ -19,10 +19,9 @@ export class MemberProfileComponent implements OnInit, OnDestroy {
             $event.preventDefault()
         }
     }
+    private accountService = inject(AccountService);
     protected memberService = inject(MemberService);
     private toast = inject(ToastService);
-    private route = inject(ActivatedRoute);
-    protected member = signal<Member | undefined>(undefined);
     protected editableMember: EditableMember = {
         displayName: '',
         description: '',
@@ -31,14 +30,11 @@ export class MemberProfileComponent implements OnInit, OnDestroy {
     };
 
     ngOnInit(): void {
-        this.route.parent?.data.subscribe(data => {
-            this.member.set(data['member']);
-        });
         this.editableMember = {
-            displayName: this.member()?.displayName || '',
-            description: this.member()?.description,
-            city: this.member()?.city || '',
-            country: this.member()?.country || ''
+            displayName: this.memberService.member()?.displayName || '',
+            description: this.memberService.member()?.description,
+            city: this.memberService.member()?.city || '',
+            country: this.memberService.member()?.country || ''
         }
     }
 
@@ -49,10 +45,20 @@ export class MemberProfileComponent implements OnInit, OnDestroy {
     }
     
     updateProfile() {
-        if(!this.member()) return;
-        const updatedMemeber = {...this.member(), ...this.editableMember};
-        console.log(updatedMemeber)
-        this.toast.success('Profile updated successfully');
-        this.memberService.editMode.set(false);
+        if(!this.memberService.member()) return;
+        const updatedMemeber = {...this.memberService.member(), ...this.editableMember};
+        this.memberService.updateMember(this.editableMember).subscribe({
+            next: () => {
+                const currentUser = this.accountService.currentUser();
+                if (currentUser && updatedMemeber.displayName !== currentUser?.displayName){
+                    currentUser.displayName = updatedMemeber.displayName;
+                    this.accountService.setCurrentUser(currentUser);
+                }
+                this.toast.success('Profile updated successfully');
+                this.memberService.editMode.set(false);
+                this.memberService.member.set(updatedMemeber as Member);
+                this.editForm?.reset(updatedMemeber);
+            }
+        });
     }
 }

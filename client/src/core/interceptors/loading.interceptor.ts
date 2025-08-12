da2718c28b18717a151e57cce180aa97fd231912
siboshvili <siboshvili.andro@gmail.com>
2025-08-12 16:09:19 +0400

    fix(client): restore caching functionality.

--- client/src/core/interceptors/loading.interceptor.ts
+++ client/src/core/interceptors/loading.interceptor.ts
@@ -1,29 +1,36 @@
-import { HttpInterceptorFn, HttpEvent } from '@angular/common/http';
+import {HttpInterceptorFn, HttpEvent, HttpParams} from '@angular/common/http';
 import {inject} from "@angular/core";
-import { BusyService } from '../services/busy.service';
+import {BusyService} from '../services/busy.service';
 import {delay, finalize, of, tap} from "rxjs";
 
 const cache = new Map<string, HttpEvent<unknown>>();
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
-  const busyService = inject(BusyService);
-  
-  // if(req.method === 'GET'){
-  //     const cachedResponse = cache.get(req.url);
-  //     if (cachedResponse) {
-  //         return of(cachedResponse)
-  //     }
-  // }
-  //
-  busyService.busy();
-  
-  return next(req).pipe(
-      delay(500),
-      tap(response => {
-         cache.set(req.url, response); 
-      }),
-      finalize(() => {
-        busyService.idle();
-      })
-  );
+    const busyService = inject(BusyService);
+
+    const generateCacheKey = (url: string, params: HttpParams): string => {
+        const paramString = params.keys().map(key => `${key}=${params.get(key)}`).join('&');
+        return paramString ? `${url}?${paramString}` : url;
+    }
+
+    const cacheKey = generateCacheKey(req.url, req.params);
+
+    if (req.method === 'GET') {
+        const cachedResponse = cache.get(cacheKey);
+        if (cachedResponse) {
+            return of(cachedResponse)
+        }
+    }
+
+    busyService.busy();
+
+    return next(req).pipe(
+        delay(500),
+        tap(response => {
+            cache.set(cacheKey, response);
+        }),
+        finalize(() => {
+            busyService.idle();
+        })
+    );
 };

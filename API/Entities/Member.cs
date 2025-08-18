085d609c645b86d6ad39d19157e2b4b9c84ab7a5
siboshvili <siboshvili.andro@gmail.com>
2025-08-18 13:15:27 +0400

    feat(messages): create message entity and migration.

--- API/Entities/Member.cs
+++ API/Entities/Member.cs
@@ -24,6 +24,12 @@ public class Member
     public List<MemberLike> LikedByMembers { get; set; } = [];
     
     [JsonIgnore]
+    public List<Message> MessagesSent { get; set; } = [];
+    
+    [JsonIgnore]
+    public List<Message> MessagesReceived { get; set; } = [];
+    
+    [JsonIgnore]
     public List<MemberLike> LikedMembers { get; set; } = [];
 
     [JsonIgnore] 

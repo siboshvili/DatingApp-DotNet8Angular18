66f1f6bcf2ead311164bd0d438f75458084aa46e
siboshvili <siboshvili.andro@gmail.com>
2025-08-20 12:39:28 +0400

    feat(message): add scroll-to-bottom functionality.

--- client/src/features/members/member-messages/member-messages.component.ts
+++ client/src/features/members/member-messages/member-messages.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit, signal} from '@angular/core';
+import {Component, effect, ElementRef, inject, OnInit, signal, ViewChild} from '@angular/core';
 import {MessageService} from '../../../core/services/message.service';
 import {MemberService} from '../../../core/services/member.service';
 import {Message} from '../../../types/message';
@@ -13,10 +13,20 @@ import {FormsModule} from "@angular/forms";
     styleUrl: './member-messages.component.css'
 })
 export class MemberMessagesComponent implements OnInit {
+    @ViewChild('messageEndRef') messageEndRef!: ElementRef;
     private messageService = inject(MessageService);
     private memberService = inject(MemberService);
     protected messages = signal<Message[]>([]);
     protected messageContent = '';
+
+    constructor() {
+        effect(() => {
+            const currentMessages = this.messages();
+            if (currentMessages.length > 0) {
+                this.scrollToBottom();
+            }
+        })
+    }
 
     ngOnInit(): void {
         this.loadMessages()
@@ -44,6 +54,14 @@ export class MemberMessagesComponent implements OnInit {
                     return [...messages, message]
                 })
                 this.messageContent = ''
+            }
+        })
+    }
+
+    scrollToBottom() {
+        setTimeout(() => {
+            if (this.messageEndRef) {
+                this.messageEndRef.nativeElement.scrollIntoView({behavior: 'smooth'})
             }
         })
     }

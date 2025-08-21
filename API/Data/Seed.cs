9ad7433bb0d57b7437aeee0431d859af4ca40e91
siboshvili <siboshvili.andro@gmail.com>
2025-08-21 14:23:46 +0400

    feat(identity): seed default admin user with roles

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -2,15 +2,16 @@ using System.Security.Cryptography;
 using System.Text.Json;
 using API.DTOs;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 public class Seed
 {
-    public static async Task SeedUsers(AppDbContext context)
+    public static async Task SeedUsers(UserManager<AppUser> userManager)
     {
-        if (await context.Users.AnyAsync()) return;
+        if (await userManager.Users.AnyAsync()) return;
 
         var memberData = await File.ReadAllTextAsync("Data/UserSeedData.json");
         var members = JsonSerializer.Deserialize<List<SeedUserDto>>(memberData);
@@ -23,8 +24,6 @@ public class Seed
 
         foreach (var member in members)
         {
-            using var hmac = new HMACSHA512();
-
             var user = new AppUser
             {
                 Id = member.Id,
@@ -53,9 +52,23 @@ public class Seed
                 MemberId = member.Id
             });
 
-            context.Users.Add(user);
+            var result = await userManager.CreateAsync(user, "Pa$$0rd");
+            if (!result.Succeeded)
+            {
+                Console.WriteLine(result.Errors.First().Description);
+            }
+
+            await userManager.AddToRoleAsync(user, "Member");
         }
 
-        await context.SaveChangesAsync();
+        var admin = new AppUser
+        {
+            UserName = "admin@test.com",
+            Email = "admin@test.com",
+            DisplayName = "Admin"
+        };
+
+        await userManager.CreateAsync(admin, "Pa$$0rd");
+        await userManager.AddToRolesAsync(admin, ["Admin", "Moderator"]);
     }
 }

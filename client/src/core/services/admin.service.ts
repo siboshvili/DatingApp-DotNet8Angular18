dd25fd2f8c72437c26f38051b164c76feb723b6c
siboshvili <siboshvili.andro@gmail.com>
2025-08-24 13:21:43 +0400

    feat(admin): add edit roles modal with update functionality.

--- client/src/core/services/admin.service.ts
+++ client/src/core/services/admin.service.ts
@@ -4,13 +4,17 @@ import {HttpClient} from "@angular/common/http";
 import {User} from '../../types/user';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AdminService {
-  baseUrl = environment.apiUrl;
-  private http = inject(HttpClient);
-  
-  getUserWithRoles(){
-    return this.http.get<User[]>(this.baseUrl + 'admin/users-with-roles')
-  }
+    baseUrl = environment.apiUrl;
+    private http = inject(HttpClient);
+
+    getUserWithRoles() {
+        return this.http.get<User[]>(this.baseUrl + 'admin/users-with-roles')
+    }
+
+    updateUserRoles(userId: string, roles: string[]) {
+        return this.http.post<string[]>(this.baseUrl + 'admin/edit-roles/' + userId + '?roles=' + roles, {});
+    }
 }

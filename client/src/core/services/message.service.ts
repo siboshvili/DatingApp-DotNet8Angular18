967ee82e2dd57efdbef72006ccbe33572615fba8
siboshvili <siboshvili.andro@gmail.com>
2025-08-26 14:22:23 +0400

    feat(message): add hub connection in message service.

--- client/src/core/services/message.service.ts
+++ client/src/core/services/message.service.ts
@@ -1,15 +1,47 @@
-import {inject, Injectable} from '@angular/core';
+import {inject, Injectable, signal} from '@angular/core';
 import {environment} from '../../environments/environment'
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {PaginatedResult} from '../../types/pagination';
 import {Message} from '../../types/message';
+import {AccountService} from '../services/account.service'
+import {HubConnection, HubConnectionBuilder, HubConnectionState} from "@microsoft/signalr";
 
 @Injectable({
     providedIn: 'root'
 })
 export class MessageService {
     private baseUrl = environment.apiUrl;
+    private hubUrl = environment.hubUrl;
     private http = inject(HttpClient);
+    private accountService = inject(AccountService);
+    private hubConnection?: HubConnection;
+    messageThread = signal<Message[]>([]);
+
+    createHubConnection(otherUserId: string) {
+        const currentUser = this.accountService.currentUser();
+        if (!currentUser) return;
+        this.hubConnection = new HubConnectionBuilder()
+            .withUrl(this.hubUrl + 'messages?userId=' + otherUserId, {
+                accessTokenFactory: () => currentUser.token
+            })
+            .withAutomaticReconnect()
+            .build();
+
+        this.hubConnection.start().catch(error => console.log(error));
+
+        this.hubConnection.on('ReceiveMessageThread', (messages: Message[]) => {
+            this.messageThread.set(messages.map(message => ({
+                ...message,
+                currentUserSender: message.senderId !== otherUserId
+            })))
+        });
+    }
+
+    stopHubConnection() {
+        if (this.hubConnection?.state === HubConnectionState.Connected) {
+            this.hubConnection.stop().catch(error => console.log(error))
+        }
+    }
 
     getMessage(container: string, pageNumber: number, pageSize: number) {
         let params = new HttpParams();
@@ -24,12 +56,12 @@ export class MessageService {
     getMessageThreed(memberId: string) {
         return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + memberId)
     }
-    
-    sendMessage(recipientId: string, content: string){
+
+    sendMessage(recipientId: string, content: string) {
         return this.http.post<Message>(this.baseUrl + 'messages', {recipientId, content})
     }
-    
-    deleteMessage(id: string){
+
+    deleteMessage(id: string) {
         return this.http.delete(this.baseUrl + 'messages/' + id);
     }
 }
